936a206ff6149fc966ece225b953efe26ef919ea
Julia <143205845+juamori@users.noreply.github.com>
2023-11-24 14:25:09 -0300

    Delete # Solicita ao usuário que insira seu nom.py

--- # Solicita ao usuário que insira seu nom.py
+++
@@ -1,8 +0,0 @@
-# Solicita ao usuário que insira seu nome
-nome = input("Digite seu nome: ")
-
-# Solicita ao usuário que insira sua senha (a entrada é convertida para inteiro)
-senha = int(input("Digite sua senha: "))
-
-# Exibe uma mensagem com base na entrada do usuário
-print("Olá, {}! Você tem {} anos.".format(nome, senha))
